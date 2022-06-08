dbf7a9b03a6daa730a3a1852e0b0301af5719a71
Jameson Stillwell <jamesonstillwell@gmail.com>
2022-06-08 09:11:02 -0400

    add in secrets

--- tests/test_placeholder.py
+++ tests/test_placeholder.py
@@ -1,3 +1,17 @@
+import os
+
+
+
 # Dummy test function
 def test_placeholder():
+    assert True
+
+
+def test_environment_secrets_working():
+    """Assert secret key is not null"""
+    try:
+        os.environ["KAGGLE_KEY"]
+    except KeyError:
+        assert False
+
     assert True
